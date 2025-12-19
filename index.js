ddda56c69b3d75fc473626d6d729bf0c2ccb43b1
Sasidhar Jonnalagadda <sasidharj1234@gmail.com>
2025-12-19 20:55:27 +0530

    feat: Implement error handling logic for invalid cities
    Updated the fetch logic to check for 404 responses, to toggle the visibility of the error message and weather container based on the API response status.

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  var data = await response.json();
-
-  console.log(data);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
 
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
@@ -27,6 +29,10 @@ async function checkWeather(city) {
   } else if (data.weather[0].main == "Mist") {
     weatherIcon.src = "https://img.icons8.com/clouds/100/000000/fog-day.png";
   }
+
+  document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+}
 }
 
 searchBtn.addEventListener("click", () => {
